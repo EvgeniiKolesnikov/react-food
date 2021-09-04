06058b17f917f515d807a286151358a75404af83
Evgenii Kolesnikov <jekow@yandex.ru>
2021-09-04 23:57:48 +0300

    Add route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
     <>
       <Header />
-      <main className="container content"></main>
+      <main className='container content'>
+        <Router>
+          <Route path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contacts' component={Contact} />
+        </Router>
+      </main>
       <Footer />
     </>
   );
