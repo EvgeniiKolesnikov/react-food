f63d6dc2e1aa7872e596aed6a473c63b7cc19d3d
Evgenii Kolesnikov <jekow@yandex.ru>
2021-09-06 23:44:48 +0300

    Add smart Search

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,10 +3,14 @@ import { useState, useEffect } from 'react';
 import Preloader from '../Preloader';
 import CategoryList from '../CategoryList';
 import Search from '../Search';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const Home = () => {
   const [catalog, setCatalog] = useState([]);
   const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+  const {pathname, search} = useLocation();
+  const {push} = useHistory();
 
   const handleSearch = (str) => {
     setFilteredCatalog(
@@ -14,14 +18,23 @@ const Home = () => {
         item.strCategory.toLowerCase().includes(str.toLowerCase())
       )
     );
+    push({
+      pathname,
+      search: `?search=${str}`
+    })
   };
 
   useEffect(() => {
     getAllCategories().then((data) => {
       setCatalog(data.categories);
-      setFilteredCatalog(data.categories);
+      setFilteredCatalog(search? data.categories.filter(item => 
+        item.strCategory
+        .toLowerCase()
+        .includes(search.split('=')[1]
+        .toLowerCase())
+      ) : data.categories);
     });
-  }, []);
+  }, [search]);
 
   return (
     <>
